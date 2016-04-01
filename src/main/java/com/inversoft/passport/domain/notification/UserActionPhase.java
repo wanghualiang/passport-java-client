38bee980e22190d9494ed4f9c3f9130dbe3d677b
Daniel DeGroff <daniel@inversoft.com>
2016-04-01 12:56:21 -0600

    Increase timeout for passport load tests.

--- src/main/java/com/inversoft/passport/domain/notification/UserActionPhase.java
+++ src/main/java/com/inversoft/passport/domain/notification/UserActionPhase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Inversoft Inc., All Rights Reserved
+ * Copyright (c) 2015-2016, Inversoft Inc., All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ public enum UserActionPhase {
   modify,
 
   /**
-   * The user action was cancelled
+   * The user action was canceled
    */
   cancel,
 
