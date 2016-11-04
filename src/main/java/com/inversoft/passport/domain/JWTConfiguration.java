de61b15d416cb92da16c70ea63c7c31b69d7f654
Daniel DeGroff <daniel@inversoft.com>
2016-11-03 19:49:18 -0600

    Working on Application Config for JWT.

--- src/main/java/com/inversoft/passport/domain/JWTConfiguration.java
+++ src/main/java/com/inversoft/passport/domain/JWTConfiguration.java
@@ -43,7 +43,7 @@ public class JWTConfiguration implements Buildable<JWTConfiguration> {
   /**
    * The length of time in seconds this JWT is valid from the time it was issued. This should be a non-zero value.
    */
-  public int timeToLiveInSeconds;
+  public int timeToLiveInSeconds = 60;
 
   public JWTConfiguration() {
   }
